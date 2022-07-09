7bded7d5fe2e70d5e994f101805f0e1612f9b540
Max Base <maxbasecode@gmail.com>
2022-07-09 20:43:34 +0430

    Clean and finish the project

--- hamiltonian-path-problem.php
+++ hamiltonian-path-problem.php
@@ -1,6 +1,12 @@
 <?php
-# Hamiltonian Path Problem
+/*
+ * Name: Hamiltonian Path Problem
+ * Description: Find a Hamiltonian path in a graph
+ * Author: Max Base
+ * Date: 2022/07/09
+ */
 
+// Calculate the distance of two points
 function d(array $point1, array $point2) : float
 {
     $x1 = $point1[0];
@@ -11,6 +17,32 @@ function d(array $point1, array $point2) : float
     return sqrt(pow($x2 - $x1, 2) + pow($y2 - $y1, 2));
 }
 
+// Optimize route from $main_city to visiting all other cities only once and return to $main_city again
+function HamiltonianPath(array $cities, array $main_city) : array
+{
+    $path = [ $main_city["name"] ];
+    $current_city = $main_city;
+    while (count($path) < count($cities)) {
+        $nearest_distance = 1000000;
+        $nearest_point = null;
+        foreach ($cities as $city) {
+            if ($city["name"] == $current_city["name"]) continue;
+            if (in_array($city["name"], $path)) continue;
+
+            $distance = d($current_city["points"], $city["points"]);
+            if ($distance < $nearest_distance) {
+                $nearest_distance = $distance;
+                $nearest_point = $city;
+            }
+        }
+        $path[] = $nearest_point["name"];
+        $current_city = $nearest_point;
+    }
+    $path[] = $main_city["name"];
+    return $path;
+}
+
+// Example
 $cities = [];
 $cities[] = ["name" => "kashan", "points" => [0, 0]];
 $cities[] = ["name" => "tehran", "points" => [1, 1]];
@@ -24,59 +56,5 @@ $cities[] = ["name" => "khoramabad", "points" => [8, 8]];
 $cities[] = ["name" => "yazd", "points" => [9, 9]];
 $cities[] = ["name" => "sari", "points" => [10, 10]];
 
-$graph = [];
-foreach($cities as $city1)
-{
-    foreach($cities as $city2)
-    {
-        if ($city1["name"] === $city2["name"]) continue;
-
-        $graph[ $city1["name"] ][ $city2["name"] ] = d($city1["points"], $city2["points"]);
-   }
-}
-
-// Optimize route from $main_point to visiting all other cities only once and return to $main_point again
-$main_point = $cities[0];
-
-function HamiltonianPath($main_point)
-{
-    global $cities;
-    global $graph;
-
-    $path = [ $main_point["name"] ];
-    $current_point = $main_point;
-    while (count($path) < count($cities))
-    {
-        $min_distance = 1000000;
-        $min_point = null;
-        foreach ($cities as $point)
-        {
-            if ($point["name"] == $current_point["name"]) continue;
-            if (in_array($point["name"], $path)) continue;
-
-            // var_dump($current_point);
-            // var_dump($point);
-            // print_r($graph[$current_point["name"]]);
-            // print_r($graph[$current_point["name"]][$point["name"]]);
-            // print "-------------\n";
-
-            // print "Checking " . $current_point["name"] . " to " . $point["name"] . "\n";
-            $distance = $graph[$current_point["name"]][$point["name"]];
-
-            if ($distance < $min_distance)
-            {
-                $min_distance = $distance;
-                $min_point = $point;
-            }
-        }
-        $path[] = $min_point["name"];
-        $current_point = $min_point;
-    }
-    $path[] = $main_point["name"];
-    return $path;
-}
-
-$res = HamiltonianPath($main_point);
+$res = HamiltonianPath($cities, $cities[0]);
 print_r($res);
-
-// print_r($graph);
